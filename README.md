f6d8e7701794d5d580da18cb089bf765854b68e7
Chaoming Wang <adaduo@outlook.com>
2024-04-03 23:10:32 +0800

    add package metadata

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center">
+  	<img alt="Header image of BrainPy - brain dynamics programming in Python." src="https://github.com/brainpy/BrainPy/blob/master/images/logo.png" width=80%>
+</p> 
+
+
+
+<p align="center">
+	<a href="https://pypi.org/project/brainscale/"><img alt="Supported Python Version" src="https://img.shields.io/pypi/pyversions/brainscale"></a>
+	<a href="https://github.com/brainpy/brainscale"><img alt="LICENSE" src="https://anaconda.org/brainpy/brainpy/badges/license.svg"></a>
+  	<a href="https://brainpy.readthedocs.io/en/latest/?badge=latest"><img alt="Documentation" src="https://readthedocs.org/projects/brainpy/badge/?version=latest"></a>
+  	<a href="https://badge.fury.io/py/brainscale"><img alt="PyPI version" src="https://badge.fury.io/py/brainscale.svg"></a>
+    <a href="https://github.com/brainpy/brainscale/actions/workflows/CI.yml"><img alt="Continuous Integration" src="https://github.com/brainpy/brainscale/actions/workflows/CI.yml/badge.svg"></a>
+</p>
+
+
+[``brainscale``](https://github.com/brainpy/brainscale) implements the core system for the next generation of [``BrainPy``](https://github.com/brainpy/BrainPy) framework. 
+
+
+
+## Installation
+
+You can install ``brainscale`` via pip:
+
+```bash
+pip install brainscale --upgrade
+```
+
+
+## Documentation
+
+The official documentation is hosted on Read the Docs: [https://brainscale.readthedocs.io](https://brainscale.readthedocs.io)
+
---
+++ setup.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import re
+import sys
+import time
+
+from setuptools import find_packages
+from setuptools import setup
+
+# version
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'brainscale/', '__init__.py'), 'r') as f:
+  init_py = f.read()
+version = re.search('__version__ = "(.*)"', init_py).groups()[0]
+if len(sys.argv) > 2 and sys.argv[2] == '--python-tag=py3':
+  version = version
+else:
+  version += '.post{}'.format(time.strftime("%Y%m%d", time.localtime()))
+
+# obtain long description from README
+with io.open(os.path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
+  README = f.read()
+
+# installation packages
+packages = find_packages(exclude=["docs*", "tests*", "examples*", "build*",
+                                  "dist*", "brainscale.egg-info*", "brainscale/__pycache__*",
+                                  "brainscale/__init__.py"])
+
+# setup
+setup(
+  name='brainscale',
+  version=version,
+  description='The Scalable Online Learning Algorithm and Framework for Biological Spiking Neural Networks.',
+  long_description=README,
+  long_description_content_type="text/markdown",
+  author='BrainPy Team',
+  author_email='chao.brain@qq.com',
+  packages=packages,
+  python_requires='>=3.9',
+  install_requires=['braincore', 'braintools'],
+  url='https://github.com/brainpy/brainscale',
+  project_urls={
+    "Bug Tracker": "https://github.com/brainpy/brainscale/issues",
+    "Documentation": "https://brainscale.readthedocs.io/",
+    "Source Code": "https://github.com/brainpy/brainscale",
+  },
+  extras_require={
+    'cpu': ['jaxlib',],
+    'cuda11': ['jaxlib[cuda11_pip]',],
+    'cuda12': ['jaxlib[cuda12_pip]',],
+    'tpu': ['jaxlib[tpu]'],
+    'cpu_mini': ['jaxlib'],
+    'cuda11_mini': ['jaxlib[cuda11_pip]'],
+    'cuda12_mini': ['jaxlib[cuda12_pip]'],
+  },
+  keywords=('computational neuroscience, '
+            'brain-inspired computation, '
+            'brain dynamics programming'),
+  classifiers=[
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Software Development :: Libraries',
+  ],
+  license='GPL-3.0 license',
+)
